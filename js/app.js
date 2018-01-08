740ed611408e42f14ce156fd9f90d92bf613ab5e
JHealy <healyj5@tcd.ie>
2018-01-08 22:52:26 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
-var app = angular.module('AddressAggregator', ['ngRoute','firebase','angular-toArrayFilter']);
+var app = angular.module('AddressAggregator', ['ngRoute', 'firebase', 'angular-toArrayFilter']);
 
 app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-	$locationProvider.hashPrefix('');
-	$routeProvider
-		.when('/',{
-			controller: 'HomeController',
-			templateUrl: 'views/home.html'
-		})
-		.when('/suggestion/:id',{
-			controller: 'SuggestionController',
-			templateUrl: '/views/suggestion.html'
-		})
-		.when('/map/:id',{
-			controller: 'MapController',
-			templateUrl: '/views/map.html'
-		})	
-		.otherwise({
-			redirectTo: '/'
-		}); 
-
-	}
-]);
+    $locationProvider.hashPrefix('');
+    $routeProvider
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'views/home.html'
+        })
+        .when('/suggestion/:id', {
+            controller: 'SuggestionController',
+            templateUrl: '/views/suggestion.html'
+        })
+        .when('/map/:id', {
+            controller: 'MapController',
+            templateUrl: '/views/map.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
